5a9bde1f19e48634a1ff1f030a895995627f73b7
Soy <soyeon.won@oxbridgeuk.com>
2025-02-16 01:22:58 +0000

    vector icons: set up

--- routes.ts
+++ routes.ts
@@ -5,6 +5,8 @@ export const RouteNames = {
   HOMETAB: 'Home-tab' as const,
 };
 
+// Define the types for the navigation stack
+// Params are not needed for the HomeTab, so we use undefined for the value of the key
 export type RootStackParamList = {
   [RouteNames.HOMETAB]: undefined; // no params needed
   [RouteNames.BROWSER]: undefined;
--- screens/ShoppingScreen.tsx
+++ screens/ShoppingScreen.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import {RouteNames, RootStackParamList} from '../routes';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+// import Icon from '@react-native-vector-icons/fontawesome';
 
 type Props = NativeStackScreenProps<RootStackParamList>;
 
+// MaterialCommunityIcons.loadFont();
 const ShoppingScreen = ({navigation}: Props) => {
   return (
     <View>
@@ -12,6 +15,8 @@ const ShoppingScreen = ({navigation}: Props) => {
       <TouchableOpacity onPress={() => navigation.navigate(RouteNames.BROWSER)}>
         <Text>Go to Browser</Text>
       </TouchableOpacity>
+      <MaterialCommunityIcons name="shopping" size={24} color="black" />
+      {/* <Icon name="rocket" size={30} color="#900" /> */}
     </View>
   );
 };
