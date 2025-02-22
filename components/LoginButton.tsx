e9f77db5f8598bf44158d70ef500ecaa06d5aff4
Soy <soyeon.won@oxbridgeuk.com>
2025-02-22 01:17:42 +0000

    logout: clear cookieAll true(webkit) webViews refresh

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,23 +1,26 @@
 import {useIsFocused, useNavigation} from '@react-navigation/native';
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useContext, useEffect, useState} from 'react';
 import {TouchableOpacity} from 'react-native';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {RootStackParamList, RouteNames} from '../routes';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import CookieManager from '@react-native-cookies/cookies';
+import {WebViewContext} from './WebViewProvider';
 
 type Props = NativeStackScreenProps<RootStackParamList>;
 
 const LoginButton = () => {
   const navigation = useNavigation<Props>();
   const isFocused = useIsFocused();
+  const context = useContext(WebViewContext);
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const iconName = isLoggedIn ? 'logout' : 'login';
 
+  console.log('isLoggedIn', isLoggedIn);
+
   useEffect(() => {
-    console.log('isFocused', isFocused);
     if (isFocused) {
       CookieManager.get('https://.naver.com', true).then(cookie => {
         if (cookie.NID_SES) {
@@ -32,7 +35,16 @@ const LoginButton = () => {
   const onPressLogin = useCallback(() => {
     navigation.navigate(RouteNames.LOGIN);
   }, [navigation]);
-  const onPressLogout = useCallback(() => {}, []);
+
+  const onPressLogout = useCallback(async () => {
+    await CookieManager.clearAll(true);
+    setIsLoggedIn(false);
+    if (context?.webViewRefs.current != null) {
+      context.webViewRefs.current.forEach(webView => {
+        webView.reload();
+      });
+    }
+  }, [context]);
 
   return (
     <TouchableOpacity onPress={isLoggedIn ? onPressLogout : onPressLogin}>
